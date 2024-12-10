4a2ee2d5bab2a863a2dd7ad4889f7b2ac46d0da6
Hekma <93189943+HekmaAr@users.noreply.github.com>
2024-12-10 10:40:00 +0100

    Update App.java xxx

--- src/main/java/com/formation/integcontinue/App.java
+++ src/main/java/com/formation/integcontinue/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "bonjour tous le monde!" );
+        System.out.println( "bonjour mariouma!" );
     }
 }
